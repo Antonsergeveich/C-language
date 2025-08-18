b0c726ca20ff2606b1575e29ffc9c9c04933f4eb
Horororrr <you@example.com>
2025-08-18 17:49:02 +0300

    565

--- 5_Arrays and strings/5.6 Two-dimensional and multi-dimensional arrays/5_6_5.c
+++ 5_Arrays and strings/5.6 Two-dimensional and multi-dimensional arrays/5_6_5.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
+//#define ONE
+#define TWO
 
 #define SIZE    10
 #define MINES   12
 
 int main(void)
 {
-    srand(time(NULL));
     char pg[SIZE][SIZE] = {0};
+    #ifdef ONE
+    int mines = MINES;
+    
+    while(mines){
+        int x = rand() % 10;
+        int y = rand() % 10;
+        int sum = 0;
+        
+        for (int i = -1; i < 2; i++){
+            for (int j = -1; j < 2; j++){
+                if (x + i >= 0 && x + i < SIZE && y + j >= 0 && y + j < SIZE){
+                    sum += pg[x + i][y + j];
+                }
+            }
+        }
+        
+        if (sum == 0){
+            pg[x][y] = '*';
+            mines--;
+        }
+    }
+    #endif
+    #ifdef TWO
     int mines = 0;
     while(mines < MINES){
         int i = rand() % SIZE;
@@ -26,12 +50,13 @@ int main(void)
             mines++;
         }
     }
-
+    
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            printf("%c ", pg[i][j] == '*' ? '*':'0');
+            printf("%c", pg[i][j] == '*' ? '*':'0');
+            if(j < SIZE -1)printf(" ");
         }
         printf("\n");
     }
-    return 0;
+    #endif
 }
