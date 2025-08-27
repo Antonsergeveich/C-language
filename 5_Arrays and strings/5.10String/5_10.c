bc9d1b49e254f3a31f00a6208b598ae2b1e1a7b8
Horororrr <you@example.com>
2025-08-27 21:47:09 +0300

    510

--- 5_Arrays and strings/5.10String/5_10.c
+++ 5_Arrays and strings/5.10String/5_10.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
 #include<string.h>
 
+//#define ONE
+#define TWO
+
 int main(void){
     char s1[12] = "Hello";
     char s2[10] = "Hello";
 
-    
+    #ifdef ONE
+    const char *str1 = s1;
+    const char *str2 = s2;
+
+    int i = 0;
+    for( ; str1[i] != '\0' && str2[i] != '\0'; ++i){
+        if(str1[i] != str2[i]){
+            puts("String are not equal!");
+            return 0;
+        }
+
+        if(str1[i] != str2[i]){
+            puts("String lengths are not equal!");
+            return 0;
+        }
+    }
+    puts("String are equal!");
+    #endif
+    #ifdef TWO
+        
+    #endif
+
     return 0;
 }
