47c08fa9c59aba9c6083fd2d309ab8d487d6ad15
Horororrr <you@example.com>
2025-08-27 21:53:30 +0300

    3

--- 5_Arrays and strings/5.10String/5_10.c
+++ 5_Arrays and strings/5.10String/5_10.c
@@ -2,7 +2,8 @@
 #include<string.h>
 
 //#define ONE
-#define TWO
+//#define TWO
+#define THREE
 
 int main(void){
     char s1[12] = "Hello";
@@ -27,8 +28,12 @@ int main(void){
     puts("String are equal!");
     #endif
     #ifdef TWO
+        int res = strcmp(s1,s2); //сравнение строк
+        if(res == 0)puts("Strings are equal!");
+        else puts("Strings are not equal!");
+    #endif
+    #ifdef THREE
         
     #endif
-
     return 0;
 }
